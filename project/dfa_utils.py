0219f35bf521de43f146ff995c65b5a18ca46219
Ivantsov Ilya <iwww68@gmail.com>
2023-03-01 20:17:01 +0300

    add task2

---
+++ project/dfa_utils.py
@@ -0,0 +1,22 @@
+import networkx as nx
+from pyformlang.regular_expression import Regex
+from pyformlang.finite_automaton import DeterministicFiniteAutomaton
+from pyformlang.finite_automaton import NondeterministicFiniteAutomaton
+
+
+def regex2dfa(expr: str) -> DeterministicFiniteAutomaton:
+    dfa = Regex(expr).to_epsilon_nfa()
+    return dfa.minimize()
+
+
+def graph2nfa(
+        graph: nx.MultiDiGraph, starts, finals
+) -> NondeterministicFiniteAutomaton:
+    nfa = NondeterministicFiniteAutomaton()
+    nfa.add_transitions([(v, d["label"], u) for v, u, d in graph.edges(data=True)])
+
+    for node in starts:
+        nfa.add_start_state(node)
+    for node in finals:
+        nfa.add_final_state(node)
+    return nfa
---
+++ tests/test_dfa_utils.py
@@ -0,0 +1,23 @@
+import pytest
+from pyformlang.finite_automaton import DeterministicFiniteAutomaton
+from project.dfa_utils import regex2dfa, graph2nfa
+from project.graph import create_two_cycles_graph
+
+
+def test_1():
+    dfa = regex2dfa("(1|0)*")
+
+    bin_dfa = DeterministicFiniteAutomaton()
+
+    bin_dfa.add_start_state(0)
+    bin_dfa.add_final_state(0)
+
+    bin_dfa.add_transitions([(0, "1", 0), (0, "0", 0)])
+
+    assert bin_dfa.is_equivalent_to(dfa)
+
+
+def test_2():
+    nfa = graph2nfa(create_two_cycles_graph(3, 3, ('1', '0')), [0], [0])
+    dfa = regex2dfa("(1 1 1 1|0 0 0 0)*")
+    assert dfa.is_equivalent_to(nfa)
