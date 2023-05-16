ff1bdc244a448cb31de932b20b4cb28c77d19ebe
Ivantsov Ilya <iwww68@gmail.com>
2023-05-16 16:13:45 +0300

    add matrix algorithm

--- project/graph.py
+++ project/graph.py
@@ -2,6 +2,9 @@ import cfpq_data
 import networkx as nx
 from collections import namedtuple
 from typing import Set, Tuple, Dict, Any, Iterable
+from scipy.sparse import dok_matrix
+
+import numpy as np
 from pyformlang.cfg.cfg import CFG, Variable
 
 from project.cfg import cfg_to_whnf
@@ -28,13 +31,13 @@ def graph_info(graph: nx.MultiDiGraph) -> GraphInfo:
 
 
 def create_two_cycles_graph(
-    n: int, m: int, labels: tuple = ("a", "b")
+        n: int, m: int, labels: tuple = ("a", "b")
 ) -> nx.MultiDiGraph:
     return cfpq_data.labeled_two_cycles_graph(n, m, labels=labels)
 
 
 def create_and_save_two_cycles_graph(
-    path: str, n: int, m: int, labels: tuple = ("a", "b")
+        path: str, n: int, m: int, labels: tuple = ("a", "b")
 ):
     save_graph(graph=create_two_cycles_graph(n=n, m=m, labels=labels), path=path)
 
@@ -84,7 +87,7 @@ def hellings_cfpq(cfg: CFG, graph: nx.MultiDiGraph) -> Set[Tuple]:
 
 
 def query_cfg_graph(
-    cfg: CFG, graph: nx.MultiDiGraph, S: Variable, starts: Iterable, finals: Iterable
+        cfg: CFG, graph: nx.MultiDiGraph, S: Variable, starts: Iterable, finals: Iterable
 ) -> Dict[Any, Set]:
     """
     Query graph representing a finite automaton with a context-free grammar
@@ -96,4 +99,54 @@ def query_cfg_graph(
     for u, var1, v in tuples:
         if var1 == S and u in starts and v in finals:
             result[u].add(v)
+    return result
+
+
+def apply_matrix_alg(cfg: CFG, graph: nx.MultiDiGraph) -> Set[Tuple]:
+    """
+    Apply matrix algorithm to a graph
+    """
+    cfg = cfg_to_whnf(cfg)
+    node_idx = {v: i for i, v in enumerate(graph.nodes)}
+    n = len(node_idx)
+    matrices = {var: dok_matrix((n, n), dtype=np.bool_) for var in cfg.variables}
+    for prod in cfg.productions:
+        if len(prod.body) == 0:
+            for i in range(n):
+                matrices[prod.head][i, i] = True
+        elif len(prod.body) == 1:
+            for u, v, symb in graph.edges.data(data="label"):
+                if Variable(symb) == prod.body[0]:
+                    i = node_idx[u]
+                    j = node_idx[v]
+                    matrices[prod.head][i, j] = True
+
+    while True:
+        matrices_changed = False
+
+        # Matrix multiplication
+        for prod in cfg.productions:
+            if len(prod.body) != 2:
+                continue
+            for var_a in matrices:
+                for var_b in matrices:
+                    if [var_a, var_b] != prod.body:
+                        continue
+
+                    for i in range(n):
+                        for j in range(n):
+                            for k in range(n):
+                                if matrices[var_a][i, j] and matrices[var_b][j, k] and not matrices[prod.head][i, k]:
+                                    matrices[prod.head][i, k] = True
+                                    matrices_changed = True
+
+        if not matrices_changed:
+            break
+
+    result = set()
+    nodes = list(graph.nodes)
+    for var in cfg.variables:
+        xs, ys = matrices[var].nonzero()
+        for i in range(len(xs)):
+            result.add((nodes[xs[i]], var, nodes[ys[i]]))
     return result
