00fbda57d7ba9938a828493f285e635eb2b63a5d
Ivantsov Ilya <iwww68@gmail.com>
2023-06-19 20:35:01 +0300

    change import

--- project/__init__.py
+++ project/__init__.py
@@ -1,1 +1,8 @@
-print("import sources directory")
+from project.cfg import *
+from project.ecfg import *
+from project.graph import *
+from project.lang.dot import *
+from project.lang.dist.LangParser import *
+from project.lang.dist.LangLexer import *
+from project.lang.dist.LangVisitor import *
+from project.lang.dist.LangListener import *
